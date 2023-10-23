1400e4a26b5d84f7baeba4130c144f289654c60d
Sten Hulsbergen <s122441@ap.be>
2023-10-23 06:01:52 +0200

    comments weg

--- Oefeningen/wifi_scanner.py
+++ Oefeningen/wifi_scanner.py
@@ -1,14 +1,11 @@
 from scapy.all import *
 
-# Functie om beacon frames te verwerken
 def handle_packet(packet):
     if packet.haslayer(Dot11Beacon):
         ssid = packet.info.decode()
         bssid = packet.addr2
         print(f"SSID: {ssid}, BSSID: {bssid}")
 
-# Draadloze interface om te scannen (pas dit aan naar jouw interface)
 interface = "wlp110s0"
 
-# Start het snifferen
 sniff(iface=interface, prn=handle_packet)
